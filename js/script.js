5046208b03ed61b162c079ca171a92b26296912b
kimlam0303 <kimlam0303@gmail.com>
2021-09-15 19:18:05 +0200

    Added Mobile Menu with JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Hello there kind stranger :) Welcome to my portfolio page!")
+
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');
+   if (x.className === 'navtoggle') {
+     x.className += ' responsive';
+   } else {
+     x.className = 'navtoggle';
+   }
+
+}
